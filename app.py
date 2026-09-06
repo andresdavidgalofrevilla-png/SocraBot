b8dc8a8efdaaf478213e0d7f897f352144e0d6f4
andresdavidgalofrevilla-png <andresdavidgalofrevilla@gmail.com>
2026-09-06 11:16:22 -0500

    Eliminado bloque __main__ para Render

--- app.py
+++ app.py
@@ -250,6 +250,5 @@ if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
 
 if __name__ == '__main__':
-    # Render usa el puerto de entorno, o 5000 por defecto
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+     port = int(os.environ.get('PORT', 5000))
+     app.run(host='0.0.0.0', port=port)
